6d735112c40af94b0cfcd1a7682d7b08aace2b10
AryanNateq <aryan.nateghnia-1@student.uts.edu.au>
2021-05-09 23:44:35 +1000

    Initialize energy-generator project.

---
+++ generator.js
@@ -0,0 +1,26 @@
+const { generator } = require('./../../ElectraJS/electra');
+
+let wattage = 45;
+let device_type = "generator";
+
+generator.listen()
+    .then((port) => {
+        console.log('Electra port opened. Listening for connections on port: ', port)
+    });
+
+generator.onConnection()
+    .then((device_data) => {
+        console.log('Connection request received: ', device_data);
+    });
+
+generator.addDevice("10.10.10.2", "4734")
+    .then((response) => {
+        console.log('Device successfully added: ', response);
+    })
+    .catch((error) => {
+        console.log('Unable to add device: ', error);
+    });
+
+// generator.sendEnergy(); // to nearest neigbour that is 'storage'.
+// generator.sendEnergy(generator.getNeighbour('A124DS2')); // By ID.
+console.log('done');
